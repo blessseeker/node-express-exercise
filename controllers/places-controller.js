f0c6be08ac5c393db1d6b4d1963d553ee9db644b
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-09-19 06:21:12 +0700

    menambahkan validasi input untuk membuat tempat baru

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -2,6 +2,8 @@ const HttpError = require("../models/http-error");
 
 const { v4: uuidv4 } = require("uuid");
 
+const { validationResult } = require("express-validator");
+
 let DUMMY_PLACES = [
   {
     id: "p1",
@@ -61,6 +63,13 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Tempat yang Anda masukkan tidak valid", 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   // const title = req.body.title;
   const createdPlace = {
