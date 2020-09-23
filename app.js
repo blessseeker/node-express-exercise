1fdd4ab3a59b52ed73b5701c9013e4353709b2c4
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-09-23 23:16:56 +0700

    koneksi ke database dengan mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -25,4 +26,13 @@ app.use((error, req, res, next) => {
     .json({ pesan: error.message || "Terjadi sebuah kesalahan" });
 });
 
-app.listen(5000);
+mongoose
+  .connect(
+    "mongodb://root:root@localhost:27017/merncourse?authSource=admin&readPreference=primary"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
