22a45d9525a57a2eaac8498dede86a7be2514977
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-09-26 00:06:59 +0700

    bersih-bersih code

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,7 +1,5 @@
 const HttpError = require("../models/http-error");
 
-const { v4: uuid } = require("uuid");
-
 const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
