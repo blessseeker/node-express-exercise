628d5da1524817f441fd01481d6a4fcfec7b6b43
Kamaludin Khoir <khoirkamaludin@gmail.com>
2020-09-26 10:48:28 +0700

    menghilangkan CORS error

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const HttpError = require("./models/http-error");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places/", placesRoutes);
 app.use("/api/users/", usersRoutes);
 
@@ -28,7 +39,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb://root:root@localhost:27017/place_db?authSource=admin&readPreference=primary"
+    "mongodb://root:root@localhost:27017/mern?authSource=admin&readPreference=primary"
   )
   .then(() => {
     app.listen(5000);
